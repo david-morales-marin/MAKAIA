07dd514d37aa4699f71b7ee8ff5b2a70c78fd958
david morales <edavid.moralesaggit config --global user.email edavid.morales@udea.edu.co>
2023-05-29 11:11:02 -0500

    se realiza el primer ejercicio del taller de vectores

--- src/main/java/org/example/taller_vectores/Ejercicio1.java
+++ src/main/java/org/example/taller_vectores/Ejercicio1.java
@@ -1,7 +1,28 @@
 package org.example.taller_vectores;
 
+import java.util.Scanner;
+
+//Crear un arreglo con n numeros, ingresados por
+// teclado y mostrar sus valores elevados al cuadrado.
 public class Ejercicio1 {
     public static void main(String[] args) {
-        System.out.println("hola mundo jajaj");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño del arreglo: ");
+        int tamano = scanner.nextInt();
+
+        int[] arreglo = new int[tamano];
+
+        System.out.println("Ingrese los elementos del arreglo:");
+        for (int i = 0; i < tamano; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            arreglo[i] = scanner.nextInt(); //
+
+        }
+        int elevado = 0;
+        for (int j = 0; j<tamano; j++){
+            elevado = arreglo[j] * arreglo[j];
+            System.out.println("El número de los elemenos al cuadrado es: " + elevado);
+        }
+
     }
 }
