1822bd735786785d34b8371706e2112ec9310c21
david morales <edavid.morales@udea.edu.co>
2023-06-26 16:58:26 -0500

    se actualiza el ejercicio de refactorizacion

---
+++ src/main/java/org/example/refactor/TipoVehiculo.java
@@ -0,0 +1,8 @@
+package org.example.refactor;
+
+public abstract class TipoVehiculo {
+
+    private int limiteInferior;
+    private int limiteInferiorIntermedio;
+    private int limiteSuperior;
+}
